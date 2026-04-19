90587248ba3593a4dce5c38821cb1ff1a8d94a8f
joaoemanuel-glitch <joaoemanuelamaro@gmail.com>
2026-04-19 14:25:40 -0300

    Update contas.py

--- banco/contas.py
+++ banco/contas.py
@@ -1,1 +1,47 @@
+from banco.operacoes import Historico, Operacao
 
+class Conta:
+    def __init__(self, numero, cliente):
+        self._numero = numero
+        self._cliente = cliente
+        self._saldo = 0.0
+        # Composição: O Histórico é criado dentro da Conta
+        self.__historico = Historico()
+
+    @property
+    def saldo(self):
+        return self._saldo
+
+    def depositar(self, valor):
+        if valor > 0:
+            self._saldo += valor
+            self.__historico.adicionar_transacao(Operacao("Depósito", valor))
+            return True
+        return False
+
+    def sacar(self, valor):
+        # Será sobrescrito nas subclasses (Polimorfismo)
+        pass
+
+    def exibir_extrato(self):
+        print(f"\n--- Extrato Conta {self._numero} ---")
+        for t in self.__historico.listar_historico():
+            print(t)
+        print(f"Saldo Atual: R$ {self._saldo:.2f}")
+
+class ContaCorrente(Conta):
+    def sacar(self, valor):
+        taxa = 2.00  # Exemplo de regra diferente para CC
+        if valor + taxa <= self._saldo:
+            self._saldo -= (valor + taxa)
+            self._Conta__historico.adicionar_transacao(Operacao("Saque (CC)", valor))
+            return True
+        return False
+
+class ContaPoupanca(Conta):
+    def sacar(self, valor):
+        if valor <= self._saldo:
+            self._saldo -= valor
+            self._Conta__historico.adicionar_transacao(Operacao("Saque (CP)", valor))
+            return True
+        return False
